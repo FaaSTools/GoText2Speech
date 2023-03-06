c0ffaa52e7215df12782a72c021320f5e4ff39d0
Dave Meyer <7davidmeyer@gmail.com>
2023-03-06 15:38:12 +0100

    Refactoring: Created new main.go file that serves the main functions used by the final user.

---
+++ GoText2Speech/main.go
@@ -0,0 +1,85 @@
+package GoText2Speech
+
+import (
+	"errors"
+	"fmt"
+	ts2_aws "goTest/GoText2Speech/aws"
+	ts2_gcp "goTest/GoText2Speech/gcp"
+	"goTest/GoText2Speech/providers"
+	. "goTest/GoText2Speech/shared"
+)
+
+// T2SDirect Transforms the given text into speech and stores the file in destination.
+// If the given options specify a provider, this provider will be used.
+// If the given options don't specify a provider, a provider will be chosen based on heuristics.
+func T2SDirect(text string, destination string, options TextToSpeechOptions) error {
+
+	// error check: If the given text is supposed to be a SSML text and does not contain <speak>-tags, it is invalid.
+	if (options.TextType == TextTypeSsml) && !HasSpeakTag(text) {
+		return errors.New("invalid text. The text type was SSML, but the given text didn't contain <speak>-tags")
+	}
+
+	// if text type is auto, text type needs to be inferred
+	if options.TextType == TextTypeAuto {
+		// SSML text needs to be wrapped in a "speak" root node (i.e. <speak>...</speak>)
+		if HasSpeakTag(text) {
+			options.TextType = TextTypeSsml
+		} else {
+			options.TextType = TextTypeText
+		}
+	}
+
+	if options.Provider == providers.ProviderUnspecified {
+		// TODO choose provider based on heuristics
+		options.Provider = providers.ProviderAWS
+	}
+
+	provider := CreateProviderInstance(options.Provider)
+	provider.CreateClient()
+
+	if options.VoiceConfig.VoiceIdConfig.IsEmpty() {
+		// if both VoiceParamsConfig is undefined -> use default object
+		if options.VoiceConfig.VoiceParamsConfig == (VoiceParamsConfig{}) {
+			options.VoiceConfig.VoiceParamsConfig = GetDefaultVoiceParamsConfig()
+		} else {
+			// if either of the properties are unset, set default values
+			if options.VoiceConfig.VoiceParamsConfig.Gender == VoiceGenderUnspecified {
+				options.VoiceConfig.VoiceParamsConfig.Gender = GetDefaultVoiceParamsConfig().Gender
+			}
+			if options.VoiceConfig.VoiceParamsConfig.LanguageCode == "" {
+				options.VoiceConfig.VoiceParamsConfig.LanguageCode = GetDefaultVoiceParamsConfig().LanguageCode
+			}
+		}
+
+		newOptions, chooseVoiceErr := provider.ChooseVoice(options)
+		if chooseVoiceErr != nil {
+			return chooseVoiceErr
+		}
+		options = newOptions
+	}
+
+	// adjust parameters for Google/AWS
+	var transformOptionsError error
+	text, options, transformOptionsError = provider.TransformOptions(text, options)
+
+	if transformOptionsError != nil {
+		return transformOptionsError
+		// TODO throw error
+	}
+
+	fmt.Println("Final Text: " + text)
+
+	t2sErr := provider.ExecuteT2SDirect(text, destination, options)
+	return t2sErr
+}
+
+func CreateProviderInstance(provider providers.Provider) T2SProvider {
+	switch provider {
+	case providers.ProviderAWS:
+		return ts2_aws.T2SAmazonWebServices{}
+	case providers.ProviderGCP:
+		return ts2_gcp.T2SGoogleCloudPlatform{}
+	default:
+		return nil
+	}
+}
--- test.go
+++ test.go
@@ -1,56 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"regexp"
 )
 
 import (
-	ts2_aws "goTest/GoText2Speech/aws"
-	ts2_gcp "goTest/GoText2Speech/gcp"
-	"goTest/GoText2Speech/providers"
+	. "goTest/GoText2Speech"
 	. "goTest/GoText2Speech/shared"
 )
 
+// main shows how T2S might be executed.
 func main() {
-	fmt.Printf("Hello World\n")
-
-	r, _ := regexp.Compile("volume=\"(?P<volume>.*)db\"")
-	str := r.FindStringSubmatch("<speak volume=\"10.00db\" >")
-	fmt.Printf("%#v: ", str)
-	fmt.Println()
-	fmt.Printf("%#v: ", r.SubexpNames())
-	fmt.Println()
-
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak volume=\"5db\">", 10))
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak volume=\"loud\">", 10))
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak rate=\"5%\" volume=\"5db\">", 10))
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak rate=\"5%\" volume=\"loud\">", 10))
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak rate=\"5%\">", 10))
-	fmt.Printf("openingTag: %s\n", IntegrateVolumeAttributeValueIntoTag("<speak>", 10))
-
-	/*
-		svc := CreatePollyClient()
-		input := &polly.DescribeVoicesInput{LanguageCode: aws.String("en-US")}
-		resp, err := svc.DescribeVoices(input)
-
-		if err != nil {
-			fmt.Printf("Error:" + err.Error())
-			os.Exit(1)
-		}
-
-		for _, v := range resp.Voices {
-			fmt.Printf("Name:\t" + *v.Name + "\n")
-			fmt.Printf("Gender:\t" + *v.Gender + "\n\n")
-		}
-	*/
 
 	fmt.Println("Synthesizing speech...")
 	s := "Hello World, how are you today? Lovely day, isn't it?"
+	destination := "testfile.mp3"
 
-	err := T2SDirect(s, "testfile.mp3", TextToSpeechOptions{
+	err := T2SDirect(s, destination, TextToSpeechOptions{
 		TextType:    TextTypeText,
 		VoiceConfig: VoiceConfig{VoiceIdConfig: VoiceIdConfig{VoiceId: "Joanna"}},
 		// Alternative voice config
@@ -66,77 +33,4 @@ func main() {
 	}
 
 	fmt.Println("Speech successfully synthesized!")
-}
-
-// T2SDirect creates text to speech input (AWS specific)
-func T2SDirect(text string, destination string, options TextToSpeechOptions) error {
-
-	// error check: If the given text is supposed to be a SSML text and does not contain <speak>-tags, it is invalid.
-	if (options.TextType == TextTypeSsml) && !HasSpeakTag(text) {
-		return errors.New("invalid text. The text type was SSML, but the given text didn't contain <speak>-tags")
-	}
-
-	// if text type is auto, text type needs to be inferred
-	if options.TextType == TextTypeAuto {
-		// SSML text needs to be wrapped in a "speak" root node (i.e. <speak>...</speak>)
-		if HasSpeakTag(text) {
-			options.TextType = TextTypeSsml
-		} else {
-			options.TextType = TextTypeText
-		}
-	}
-
-	if options.Provider == providers.ProviderUnspecified {
-		// TODO choose provider based on heuristics
-		options.Provider = providers.ProviderAWS
-	}
-
-	provider := CreateProviderInstance(options.Provider)
-	provider.CreateClient()
-
-	if options.VoiceConfig.VoiceIdConfig.IsEmpty() {
-		// if both VoiceParamsConfig is undefined -> use default object
-		if options.VoiceConfig.VoiceParamsConfig == (VoiceParamsConfig{}) {
-			options.VoiceConfig.VoiceParamsConfig = GetDefaultVoiceParamsConfig()
-		} else {
-			// if either of the properties are unset, set default values
-			if options.VoiceConfig.VoiceParamsConfig.Gender == VoiceGenderUnspecified {
-				options.VoiceConfig.VoiceParamsConfig.Gender = GetDefaultVoiceParamsConfig().Gender
-			}
-			if options.VoiceConfig.VoiceParamsConfig.LanguageCode == "" {
-				options.VoiceConfig.VoiceParamsConfig.LanguageCode = GetDefaultVoiceParamsConfig().LanguageCode
-			}
-		}
-
-		newOptions, chooseVoiceErr := provider.ChooseVoice(options)
-		if chooseVoiceErr != nil {
-			return chooseVoiceErr
-		}
-		options = newOptions
-	}
-
-	// adjust parameters for Google/AWS
-	var transformOptionsError error
-	text, options, transformOptionsError = provider.TransformOptions(text, options)
-
-	if transformOptionsError != nil {
-		return transformOptionsError
-		// TODO throw error
-	}
-
-	fmt.Println("Final Text: " + text)
-
-	t2sErr := provider.ExecuteT2SDirect(text, destination, options)
-	return t2sErr
-}
-
-func CreateProviderInstance(provider providers.Provider) T2SProvider {
-	switch provider {
-	case providers.ProviderAWS:
-		return ts2_aws.T2SAmazonWebServices{}
-	case providers.ProviderGCP:
-		return ts2_gcp.T2SGoogleCloudPlatform{}
-	default:
-		return nil
-	}
 }
